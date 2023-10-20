70707f44a7059f20f93fccbf9dab6554a1277d5c
davide-rullo <davide.rullo01@gmail.com>
2023-10-20 15:29:22 +0200

    add classes and layout

--- index.php
+++ index.php
@@ -1,10 +1,38 @@
 <?php
 class product
 {
-    public $title;
+    public $name;
     public $price;
     public $image;
+    public function __construct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
 }
+
+class category
+{
+    public $race;
+}
+
+class food extends product
+{
+    public $type;
+}
+
+class toy extends product
+{
+    public $type;
+}
+
+class kennel extends product
+{
+    public $size;
+}
+
+
+$toy1 = new Toy('palla', 4);
 
 ?>
 
@@ -15,10 +43,36 @@ class product
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Animal Store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
 
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Animal Store</a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-img">
+                            <div class="card-body">Prova</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
