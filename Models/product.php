de21b7d7f3ceb560cd13c886dea1b13d897c2cc9
davide-rullo <davide.rullo01@gmail.com>
2023-10-23 16:00:04 +0200

    add exception

--- Models/product.php
+++ Models/product.php
@@ -20,4 +20,34 @@ class product
         $this->category = $category;
         $this->productType = $productType;
     }
+
+    public function insertPrice($addedPrice)
+    {
+
+        if (!is_numeric($addedPrice)) {
+
+
+
+            throw new InvalidArgumentException('It\'s not a number. The function needs a number to work.', 1);
+        } elseif ($addedPrice > 1000) {
+            //throw new Exception('Invalid price range!');
+            throw new RangeException('Invalid price range!');
+        }
+        return $addedPrice;
+    }
 }
+
+/* 
+$exampleProduct = new Product('Osso', 20, 'image', 'categ', 'pt');
+
+try {
+    $newPrice = $exampleProduct->insertPrice('1200');
+    echo $newPrice;
+} catch (Exception $error) {
+    echo $error->getMessage();
+    echo '<br>';
+    echo $error->getFile();
+    echo '<br>';
+    echo $error->getTraceAsString();
+}
+*/
